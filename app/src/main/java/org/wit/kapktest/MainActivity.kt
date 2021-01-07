d73c4769434cb5a3915c5d08dfda44d95b60b840
Marcel S <49673844+ProgrammiererNep@users.noreply.github.com>
2021-01-07 13:00:16 +0100

    feature 1: Anko Logging

--- app/src/main/java/org/wit/kapktest/MainActivity.kt
+++ app/src/main/java/org/wit/kapktest/MainActivity.kt
@@ -4,14 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import org.wit.kapktest.R
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.info
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.toast
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), AnkoLogger {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         button.setOnClickListener() {
-            setContentView(R.layout.activity_test2)
+            //setContentView(R.layout.activity_test2)
+            info("New feature 1 added")
         }
 
 
